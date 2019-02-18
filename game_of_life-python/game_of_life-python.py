5a0a70aa28e18adb003ec42404f1fc28d5526315
NicEastvillage <nicoesterby@gmail.com>
2019-02-18 11:45:55 +0100

    Added comments to python cgol example.

--- game_of_life-python/game_of_life-python.py
+++ game_of_life-python/game_of_life-python.py
@@ -3,13 +3,13 @@ import pygame
 from pygame.locals import *
 from random import randint
 
-
+# Constants
 WORLD_SIZE = 120
 CELL_SIZE = 6
-
 DEAD = 0
 ALIVE = 1
 
+# Init pygame and world grid
 pygame.init()
 screen = pygame.display.set_mode((WORLD_SIZE * CELL_SIZE, WORLD_SIZE * CELL_SIZE))
 world = [[randint(0, 1) for x in range(WORLD_SIZE)] for y in range(WORLD_SIZE)]
@@ -18,6 +18,7 @@ clock = pygame.time.Clock()
 
 
 def render():
+	""" Renders the world to the screen """
 	for x in range(WORLD_SIZE):
 		for y in range(WORLD_SIZE):
 			color = (255, 255, 255) if world[x][y] == DEAD else (0, 0, 0)
@@ -25,10 +26,12 @@ def render():
 
 
 def count_alive_neighbours(x, y):
+	""" Returns the number of moore neighbours around cell (x, y) that are alive """
 	count = 0
 	for dx in (-1, 0, 1):
 		for dy in (-1, 0, 1):
 			if dx != 0 or dy != 0:
+				# Using modulo the grid wraps around at edges
 				cell_state = world[(x + dx) % WORLD_SIZE][(y + dy) % WORLD_SIZE]
 				if cell_state == ALIVE:
 					count += 1
@@ -36,6 +39,7 @@ def count_alive_neighbours(x, y):
 
 
 def get_next_state(state, alive_neighbours):
+	""" Given the state of a cell and the number of alive neighbours, this uses the rules of CGOL to find the next state """
 	if state == ALIVE:
 		if 2 <= alive_neighbours <= 3:
 			return ALIVE
@@ -49,6 +53,7 @@ def get_next_state(state, alive_neighbours):
 			return DEAD
 
 def update():
+	""" Iterates through each cell of the world grid and updates the world configuration """
 	global world
 	new_world = [[0]*WORLD_SIZE for i in range(WORLD_SIZE)]
 	
@@ -60,17 +65,21 @@ def update():
 	world = new_world
 
 
+# Main loop
 while running:
     for event in pygame.event.get():
+        # Close window checks
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
         elif event.type == QUIT:
             running = False
-
+            
+    # Main logic
     render()
     pygame.display.flip()
     pygame.time.delay(5)
     update()
 
+# Close pygame correctly
 pygame.quit()
