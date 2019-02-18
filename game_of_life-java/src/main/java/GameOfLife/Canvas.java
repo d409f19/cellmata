9a436124d9f7b85709a1c09863ec8e82e94816e3
cogitantium <falke.cs@gmail.com>
2019-02-18 11:55:13 +0100

    refactored and commented source, added further checks, now fully functional CGOL

--- game_of_life-java/src/main/java/GameOfLife/Canvas.java
+++ game_of_life-java/src/main/java/GameOfLife/Canvas.java
@@ -7,47 +7,57 @@ class Canvas {
     private final int x;
     private final int y;
     private Cell[][] canvas;
-    private final Cell[][] tempCanvas;
+    private Cell[][] tempCanvas;
 
     Canvas(int x, int y) {
         this.x = x;
         this.y = y;
+        // set two canvasses up, such that one can be calculated from,
+        // and the resulting state can be stored in the other
         this.canvas = new Cell[x][y];
         this.tempCanvas = new Cell[x][y];
         // seed canvas with randomness
         Random random = new Random(new Date().getTime());
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                this.canvas[i][j] = new Cell(random.nextInt(20) == 0);
+                // 50/50 chance of each cell being alive at initial configuration
+                this.canvas[i][j] = new Cell(random.nextInt(2) == 0);
+                // initialise tempCanvas to false, doesn't affect runtime, but each cell needs to have a Cell
+                this.tempCanvas[i][j] = new Cell(false);
             }
         }
     }
 
     void tick() {
         int neighbours;
+        // temp canvas-array for swapping before return
+        Cell[][] temp;
         for (int i = 0; i < this.x; i++) {
             for (int j = 0; j < this.y; j++) {
                 neighbours = countMooreNeighbours(i, j);
 
-                // deep copy cell
-                tempCanvas[i][j] = canvas[i][j];
-
-                // set new state based on the life of the cell
+                // set new state in tempCanvas based on the life of the given cell in canvas
                 tempCanvas[i][j].setState(cellLife(canvas[i][j], neighbours));
             }
         }
+        // swap references to arrays
+        temp = canvas;
         canvas = tempCanvas;
+        tempCanvas = temp;
     }
 
     private int countMooreNeighbours(int x, int y) {
         int neighbours = 0;
+        // iterate nearest eight neighbours
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
+                // set canvas position to actual
                 int xPos = x + i;
                 int yPos = y + j;
 
-                if (!(xPos < 0 || xPos >= this.x || yPos < 0 || yPos >= this.y)) {
-                    // bound coordinate within constraints
+                // don't count neighbours of a given cell, by short-circuiting, or if out of bounds
+                if (!(i == 0 && j == 0 || xPos < 0 || xPos >= this.x || yPos < 0 || yPos >= this.y)) {
+                    // if neighbour is alive, count up neighbours
                     if (this.canvas[x + i][y + j].isAlive())
                         neighbours++;
                 }
@@ -57,20 +67,14 @@ class Canvas {
     }
 
     private boolean cellLife(Cell cell, int neighbours) {
+        // if cell is alive, and has between 2 and 3 alive neighbours, stay alive, else die
         if (cell.isAlive()) {
             return neighbours >= 2 && neighbours <= 3;
+        // if cell is dead, become alive if it has 3 neighbours, else stay dead
         } else return neighbours == 3;
     }
 
-    public Cell[][] getCanvas() {
+    Cell[][] getCanvas() {
         return canvas;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 }
