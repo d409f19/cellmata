debea20042d1ee73501b75e8fccd180026098527
NicEastvillage <nicoesterby@gmail.com>
2019-03-19 14:18:57 +0100

    Added Types and functionality for implicit type conversion.

--- compiler/src/main/kotlin/ast/types.kt
+++ compiler/src/main/kotlin/ast/types.kt
@@ -7,4 +7,29 @@ object FloatType : Type()
 object BooleanType : Type()
 object StateType : Type()
 object ActualNeighbourhoodType : Type() // An evaluated neighbourhood
-data class ArrayType(val subtype: Type)
+data class ArrayType(val subtype: Type) : Type()
+
+object UncheckedType : Type() // Type of expressions until type checking is done
+
+fun typeFromString(str: String) {
+    when (str) {
+        "int" -> IntegerType
+        "float" -> FloatType
+        "bool" -> BooleanType
+        "state" -> StateType
+        else -> error("Type not recognised.") // TODO Replace with smarter error reporting
+    }
+}
+
+fun wrapInImplicitConversion(expr: Expr, targetType: Type) : Expr {
+    // There are two types of implicit type conversions:
+    // int -> float
+    // array<state> -> actual neighbourhood
+
+    val type = expr.type
+    return when {
+        type is IntegerType && targetType is FloatType -> IntToFloatConversion(expr)
+        type is ArrayType && type.subtype is StateType && targetType is ActualNeighbourhoodType -> StateArrayToActualNeighbourhoodConversion(expr)
+        else -> error("Cannot implicitly convert $type to $targetType.") // TODO Replace with smarter error reporting
+    }
+}
