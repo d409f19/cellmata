8c7626f181bc6b8845e4303c35f842e1df7e30e3
Jacob Jenner Rasmussen <jacob.mindstorms@gmail.com>
2019-03-19 12:27:14 +0100

    Added simple test
    Added a test that runs the entire compile on the stress.cell example

---
+++ compiler/src/test/kotlin/test.kt
@@ -0,0 +1,23 @@
+package cs.aau.dk.d409f19
+
+import cs.aau.dk.d409f19.antlr.CellmataLexer
+import cs.aau.dk.d409f19.antlr.CellmataParser
+import dk.aau.cs.d409f19.cellumata.ast.visit
+import org.antlr.v4.runtime.ANTLRFileStream
+import org.antlr.v4.runtime.CommonTokenStream
+import org.junit.jupiter.api.Test
+
+class CompilerTests {
+
+    @Test
+    fun `Run compiler`() {
+        val inputStream = ANTLRFileStream("src/main/resources/stress.cell")
+        val lexer = CellmataLexer(inputStream)
+        val tokenStream = CommonTokenStream(lexer)
+        val parser = CellmataParser(tokenStream)
+
+        val startContext = parser.start()
+
+        val ast = visit(startContext)
+    }
+}
