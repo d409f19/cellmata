0df50a77738982cc9fe10742e341cc6dc0148f8d
cogitantium <falke.cs@gmail.com>
2019-04-03 15:07:58 +0200

    added boilerplate creation functions and test for defaults in boilerplate

--- compiler/src/test/kotlin/operations.kt
+++ compiler/src/test/kotlin/operations.kt
@@ -1,21 +1,21 @@
 package dk.aau.cs.d409f19
 
 import dk.aau.cs.d409f19.cellumata.ErrorLogger
-import dk.aau.cs.d409f19.cellumata.ast.reduce
+import dk.aau.cs.d409f19.cellumata.ast.*
 import dk.aau.cs.d409f19.cellumata.walkers.LiteralExtractorVisitor
 import dk.aau.cs.d409f19.cellumata.walkers.ScopeCheckVisitor
 import dk.aau.cs.d409f19.cellumata.walkers.TypeChecker
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class OperationTests {
-    
+
     /**
      * Compiles a program given as string parameter, asserts that the given program compiles successfully
      */
-    fun CompileProgram(program: String) {
+    fun CompileProgram(program: String): AST {
         val source = CharStreams.fromString(program)
         val lexer = dk.aau.cs.d409f19.antlr.CellmataLexer(source)
         val tokenStream = CommonTokenStream(lexer)
@@ -43,7 +43,8 @@ class OperationTests {
         ErrorLogger.assertNoErrors()
 
         // Assert that no errors occured
-        Assertions.assertFalse(ErrorLogger.hasErrors())
+        assertFalse(ErrorLogger.hasErrors())
+        return ast
     }
 
     /**
@@ -54,26 +55,96 @@ class OperationTests {
         val lexer = dk.aau.cs.d409f19.antlr.CellmataLexer(source)
         val tokenStream = CommonTokenStream(lexer)
         return dk.aau.cs.d409f19.antlr.CellmataParser(tokenStream)
-
     }
 
     /**
-     * Returns a boilerplate program with optionally inserted line
+     * Returns a world declaration based on given parameters and with sane defaults
+     * TODO: should handle optional, second dimension - as of now, it's required
      */
-    fun getBoilerplate(source: String? = ""): String {
-        return "world { size = 10[wrap], 20[wrap] tickrate = 120 cellsize = 5} $source state foo (0,0,0) { let y = 42; become foo; }"
+    fun getWorldDecl(
+        dimOneSize: Int = 10,
+        dimOneType: String = "wrap",
+        dimTwoSize: Int = 20,
+        dimTwoType: String = "wrap",
+        tickrate: Int = 120,
+        cellsize: Int = 5
+    ): String {
+        return """world {
+            |  size = $dimOneSize[$dimOneType], $dimTwoSize[$dimTwoType]
+            |  tickrate = $tickrate
+            |  cellsize = $cellsize
+            |}
+            |
+            | """.trimMargin()
     }
 
     /**
-     * Compile boilerplate program
+     * Returns a constant declaration based on given parameters and with sane defaults
+     */
+    fun getConstDecl(ident: String = "ident", value: String = "false"): String {
+        return "const $ident = $value;"
+    }
+
+    /**
+     * Returns a state array declaration based on given parameters and with sane defaults
+     * TODO: stateArray is default empty as compiler on master-branch did not support this yet when branching out
+     */
+    fun getStateDecl(
+        ident: String = "stage",
+        stateArray: String = "",
+        red: Int = 255,
+        green: Int = 200,
+        blue: Int = 100,
+        body: String = "",
+        become: String = "become $ident;"
+    ): String {
+        return """state $ident $stateArray ($red, $green, $blue) {
+            |  $body
+            |  $become
+            |}
+            |""".trimMargin()
+    }
+
+    /**
+     * Returns a boilerplate program
+     */
+    fun getBoilerplate(): String {
+        return getWorldDecl() + getConstDecl() + "\n\n" + getStateDecl()
+    }
+
+    /**
+     * Compile boilerplate program from defaults
      */
     @Test
     fun BoilerplateTest() {
-        CompileProgram(getBoilerplate())
-    }
+        val program = getBoilerplate()
+        print(program)
+        val ast = CompileProgram(program)
+        // Casting to rootnode to access fields
+        val rootNode = ast as RootNode
+        // Todo: Should be refactored to actual values when compiler gains ability
+        assertNull(rootNode.world.cellSize)
+        assertNull(rootNode.world.tickrate)
 
-    @Test
-    fun AdditionTest1() {
-        CompileProgram(getBoilerplate("const bar = 2 + 42;"))
+        // First body of rootnode should be constant declaration
+        val const = rootNode.body.get(0) as ConstDecl
+
+
+        assertEquals("ident", const.ident)
+        val constExpr = const.expr as BoolLiteral
+        assertFalse(constExpr.value)
+        assertTrue(const.type is BooleanType)
+
+        // Second body of rootnode should be foo state declaration
+        val state = rootNode.body.get(1) as StateDecl
+        assertEquals("stage", state.ident)
+
+        assertEquals(255, state.red)
+        assertEquals(200, state.green)
+        assertEquals(100, state.blue)
+
+        val becomeStmt = state.body.get(0) as BecomeStmt
+        val becomeStmtNamedExpr = becomeStmt.state as NamedExpr
+        assertEquals("stage", becomeStmtNamedExpr.ident)
     }
 }
