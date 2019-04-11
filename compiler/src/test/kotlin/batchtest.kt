4f72ff95fce72c02262d1b380f7c890722f93f7b
cogitantium <falke.cs@gmail.com>
2019-04-11 13:32:49 +0200

    refactored batchtesting to contain program name and emit TCE exception with name in msg on error

--- compiler/src/test/kotlin/batchtest.kt
+++ compiler/src/test/kotlin/batchtest.kt
@@ -1,6 +1,8 @@
 package dk.aau.cs.d409f19
 
 import dk.aau.cs.d409f19.Utilities.Companion.compileProgram
+import dk.aau.cs.d409f19.cellumata.ErrorLogger
+import dk.aau.cs.d409f19.cellumata.TerminatedCompilationException
 import org.junit.jupiter.api.Test
 import java.io.File
 
@@ -14,22 +16,32 @@ class BatchTest {
     @Test
     fun batchPass() {
         batchDir.getPrograms().forEach {
-            compileProgram(it)
+            ErrorLogger.reset()
+            compileProgram(it.program)
+            if (ErrorLogger.hasErrors()) {
+                ErrorLogger.printAllErrors()
+                throw TerminatedCompilationException("Errors occured in program: " + it.filename)
+            }
         }
     }
+
+    /**
+     * Class for holding a filename and a single program in a single string
+     */
+    data class CellmataProgram(val filename: String, val program: String)
 
     /**
      * Returns a list of strings from each '.cell' file under a given directory.
      * Since this function is a class-extension of string, but private, which takes a string as a receiver,
      * this only alters functionality of strings under this class
      */
-    private fun String.getPrograms(): List<String> {
-        val list = mutableListOf<String>()
+    private fun String.getPrograms(): List<CellmataProgram> {
+        val list = mutableListOf<CellmataProgram>()
         // Walk top-down
         File(this).walk().forEach {
             // If it is a file and has extension 'cell', then add source to list as string
             if (it.isFile && it.extension == "cell") {
-                list.add(it.readText())
+                list.add(CellmataProgram(it.name, it.readText()))
             }
         }
         return list
