2b11076da146849070fed4d6ae9c1937c17c445d
cogitantium <falke.cs@gmail.com>
2019-04-14 15:43:15 +0200

    removed unnecessary import statement

--- compiler/src/test/kotlin/symboltest.kt
+++ compiler/src/test/kotlin/symboltest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.opentest4j.AssertionFailedError
-import java.lang.NullPointerException
 
 class SymbolTest {
 
