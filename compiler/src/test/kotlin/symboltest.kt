3cacee92291532c9185a91e024ec3f3764de424e
cogitantium <falke.cs@gmail.com>
2019-04-16 10:36:25 +0200

    major overhaul of symboltest.kt, refactored to be parameterised

--- compiler/src/test/kotlin/symboltest.kt
+++ compiler/src/test/kotlin/symboltest.kt
@@ -3,78 +3,89 @@ package dk.aau.cs.d409f19
 import dk.aau.cs.d409f19.cellumata.ErrorLogger
 import dk.aau.cs.d409f19.cellumata.ast.*
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.opentest4j.AssertionFailedError
+import java.util.stream.Stream
 
+@TestInstance(PER_CLASS)
+@DisplayName("Symbol table tests")
 class SymbolTest {
 
     /**
-     * Reset ErrorLogger as it is globally persistent and will interfere with tests
+     * Returns list of values for testing right-hand-side of assignment statement.
+     * TODO: Does not include negative values as they fail! When fixed, they should be implemented again.
      */
-    @BeforeEach
-    fun resetErrorLogger() {
-        ErrorLogger.reset()
+    fun assignStmtPassData(): List<String> {
+        return IntRange(-50, 50).step(10).map { it.toString() }
+            .union(listOf("true", "false", "113240987.734723984", "1.0000000000000001"))
+            .toList()
     }
 
     /**
      * Tests assignments statements which should be legal, given an identifier and value as parameter.
-     * If assertions fails, exceptions are caught, and false is returned to indicate a failed test to caller.
      */
-    private fun <T> assignStmtPass(ident: String, value: T): Boolean {
+    @ParameterizedTest
+    @MethodSource("assignStmtPassData")
+    fun assignStmtPassTest(value: String) {
 
         val compilerData =
-            compileProgram(getWorldDeclString() + "\n\n" + getStateDeclString(body = "let $ident = $value;"))
+            compileProgram(getWorldDeclString() + "\n\n" + getStateDeclString(body = "let x = $value;"))
 
         // Get SymbolTable for first subscope, which is first StateDecl
         val stateSymbolTable = compilerData.symbolTable.tables[0]
 
-        try {
-            // Assert identifier of variable is contained in first symbol scope
-            assertTrue(stateSymbolTable.symbols.containsKey(ident))
 
-            // Get expression of AssignStmt which is value of the key of ident. Will
-            val literal = (stateSymbolTable.symbols[ident] as AssignStmt).expr
+        // Assert identifier of variable is contained in first symbol scope
+        assertTrue(stateSymbolTable.symbols.containsKey("x"))
 
-            // Switch on literal type, when either three assignable literals, assert equal value, else throw exception
-            when (literal) {
-                is IntLiteral -> assertEquals(value, literal.value)
-                is BoolLiteral -> assertEquals(value, literal.value)
-                is FloatLiteral -> assertEquals(value, literal.value)
-                else -> throw AssertionFailedError("Type was not of assignable type! Was " + literal.getType())
-            }
-        } catch (e: AssertionFailedError) { // If any assertion errors thrown, catch and return false
-            return false
+        // Switch on literal type, when either three assignable literals, assert equal value, else throw exception
+        when (val literal = (stateSymbolTable.symbols["x"] as AssignStmt).expr) {
+            is IntLiteral -> assertEquals(value.toInt(), literal.value)
+            is BoolLiteral -> assertEquals(value.toBoolean(), literal.value)
+            is FloatLiteral -> assertEquals(value.toFloat(), literal.value, 0.00000000000000000001f)
+            else -> throw AssertionFailedError("Type was not of assignable type! Was " + literal.getType())
         }
-        return true
     }
 
     /**
-     * Dispatches assignment statement tests
+     * Returns the list of reserved words for testing data. Note that this cannot be passed to test by:
+     * @ValueSource("strings = RESERVED_WORDS") as JUnit5 complains that the source is not compile-time static
      */
-    @Test
-    fun assignStmtTest() {
-        for (i in -50..50) assertTrue(assignStmtPass("x", i))
-        assertTrue(assignStmtPass("floatingPoint", 1636098.1239487.toFloat()))
-        assertTrue(assignStmtPass("falseBool", false))
-        assertTrue(assignStmtPass("trueBool", true))
+    fun parserReservedSymbolsData(): List<String> {
+        return RESERVED_WORDS
     }
 
     /**
      * Tests whether parser recognises syntax errors on reserved words from symbol table
      */
-    @Test
-    fun parserReservedSymbolsTest() {
-        RESERVED_WORDS.forEach {
-            val parser = getParser(getWorldDeclString() + getStateDeclString(ident = it))
-            // Stop parser from printing errors to stderr for less noisy console
-            parser.removeErrorListeners()
-            // Parse program
-            parser.start()
+    @ParameterizedTest
+    @MethodSource("parserReservedSymbolsData")
+    fun parserReservedSymbolsTest(identifier: String) {
+        val parser = getParser(getWorldDeclString() + getStateDeclString(ident = identifier))
+        // Stop parser from printing errors to stderr for less noisy console
+        parser.removeErrorListeners()
+        // Parse program
+        parser.start()
 
-            // Assert that syntax-errors are recognised for reserved words
-            assertTrue(parser.numberOfSyntaxErrors > 0, "Test failed on: $it")
-        }
+        // Assert that syntax-errors are recognised for reserved words
+        assertTrue(parser.numberOfSyntaxErrors > 0, "Test failed on: $identifier")
+    }
+
+    /**
+     * Returns a list of arguments, which are an identifier and a list of strings
+     */
+    fun symbolRedefinitionFailData(): Stream<Arguments> {
+        return Stream.of(
+            Arguments.of("x", listOf("4", "2")),
+            Arguments.of("x", listOf(true.toString(), false.toString())),
+            Arguments.of("x", listOf(4.22.toString(), 42.2.toString())),
+            Arguments.of("x", listOf("4", "2+2"))
+        )
     }
 
     /**
@@ -86,58 +97,34 @@ class SymbolTest {
      * @param ident Identifier to use for each assignment statement
      * @param values List of values which will be assigned to given identifier
      */
-    fun <T> symbolRedefinition(ident: String, values: List<T>): Boolean {
+    @ParameterizedTest
+    @MethodSource("symbolRedefinitionFailData")
+    fun symbolRedefinitionFailTest(ident: String, values: List<String>) {
         val stringBuilder = StringBuilder()
         // For each value, create assignment expression with equal identifier and given value
         values.forEach {
-            stringBuilder.appendln("let $ident = ${it.toString()};")
+            stringBuilder.appendln("let $ident = $it;")
         }
 
         // Compile boilerplate program with state having the constructed body
         compileProgram(getWorldDeclString() + getStateDeclString(body = stringBuilder.toString()))
 
-        try {
-            // Assert that errors are registered
-            assertTrue(ErrorLogger.hasErrors())
-
-            // For each error recorded, assert that error is of SymbolRedefinitionError-type and with given identifier
-            ErrorLogger.allErrors().forEach {
+        // For each error recorded, assert that error is of SymbolRedefinitionError-type and with given identifier
+        ErrorLogger.allErrors().forEach {
+            try {
                 assertTrue(
                     it is SymbolRedefinitionError,
                     "Class assertion error at: $it"
-                ) // If class is not a SRE, ASE is thrown with message on error if 'is'-keyword assertion fails
+                ) /* If class is not a SymbolRedefinitionError,
+                     AssertionFailedException is thrown with message on error if 'is'-keyword assertion fails */
                 assertTrue(
                     (it as SymbolRedefinitionError).ident == ident,
                     "Identifier error at: $it"
-                ) /* If identifier of SRE, which is the ident of the given symbol which is redefined,
-                     is not equal to the actual ident passed, then assertion fails */
+                ) /* If identifier of SymbolRedefinitionError, which is the identifier of the given symbol
+                     which is redefined, is not equal to the actual identifier passed, then assertion fails */
+            } catch (e: AssertionFailedError) {
+                fail("AssertionFailedError: $it was not a SymbolRedefinitionError")
             }
-        } catch (e: AssertionFailedError) {
-            return false
         }
-        return true
     }
-
-    /**
-     * Dispatches symbolRedefinition test on given parameter which should fail
-     */
-    @Test
-    fun symbolRedefinitionFailTest() {
-        assertTrue(
-            symbolRedefinition("a", listOf(4, 2, true, 42.2.toFloat(), false)),
-            "Test failed trying to redefine symbol"
-        )
-    }
-
-    /**
-     * Dispatches symbolRedefinition test on given parameter which should pass
-     */
-    @Test
-    fun symbolRedefinitionPassTest() {
-        assertFalse(symbolRedefinition("x", listOf(2)), "Test succeeded when expecting fail")
-        assertFalse(symbolRedefinition("y", listOf(false)), "Test succeeded when expecting fail")
-        assertFalse(symbolRedefinition("z", listOf(42.0.toFloat())), "Test succeeded when expecting fail")
-    }
-
-
 }
