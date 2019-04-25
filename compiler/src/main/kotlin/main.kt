ab6b3990e19af45ccac7554459dc4be3aa56994d
cogitantium <falke.cs@gmail.com>
2019-04-25 14:30:18 +0200

    refactored main compile functions

--- compiler/src/main/kotlin/main.kt
+++ compiler/src/main/kotlin/main.kt
@@ -8,6 +8,7 @@ import dk.aau.cs.d409f19.cellumata.ast.reduce
 import dk.aau.cs.d409f19.cellumata.visitors.SanityChecker
 import dk.aau.cs.d409f19.cellumata.visitors.ScopeCheckVisitor
 import dk.aau.cs.d409f19.cellumata.visitors.TypeChecker
+import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import java.nio.file.Path
@@ -15,34 +16,65 @@ import java.nio.file.Paths
 
 val path = Paths.get("src/main/resources/compiling-programs/skeleton.cell")
 
-fun compile(path: Path) {
+fun main() {
+    // Compile static program
+    prodCompilation(path)
+}
+
+/**
+ * Compile program from given source at path
+ */
+fun compileSource(source: Path): CompilerData {
+    // Actually compile program and return compiler-data
+    return compile(CharStreams.fromPath(source))
+}
+
+/**
+ * Compile program from given source string
+ */
+fun compileSource(source: String): CompilerData {
+    // Actually compile program and return compiler-data
+    return compile(CharStreams.fromString(source))
+}
+
+/**
+ * Compile program from given CharStream as input
+ */
+fun compile(inputStream: CharStream): CompilerData {
+    val lexer = CellmataLexer(inputStream)
+    val tokenStream = CommonTokenStream(lexer)
+    val parser = CellmataParser(tokenStream)
+
+    // Build AST
+    val startContext = parser.start()
+    val ast = reduce(startContext)
+    // Asserts that no errors has been found during the last phase
+    ErrorLogger.assertNoErrors()
+
+    // Sanity checker
+    val sanityChecker = SanityChecker()
+    sanityChecker.visit(ast)
+
+    // Symbol table and scope
+    val scopeChecker = ScopeCheckVisitor()
+    scopeChecker.visit(ast)
+    val symbolTable = scopeChecker.getSymbolTable()
+    println(symbolTable)
+    ErrorLogger.assertNoErrors()
+
+    // Type checking
+    TypeChecker(symbolTable).visit(ast)
+    ErrorLogger.assertNoErrors()
+
+    return CompilerData(parser, ast, symbolTable)
+}
+
+/**
+ * Production compile function, outputs useful information to user on errors in source and possibly the compiler itself
+ */
+fun prodCompilation(path: Path) {
     try {
-        val inputStream = CharStreams.fromPath(path)
-        val lexer = CellmataLexer(inputStream)
-        val tokenStream = CommonTokenStream(lexer)
-        val parser = CellmataParser(tokenStream)
-
-        // Build AST
-        val startContext = parser.start()
-        val ast = reduce(startContext)
-        // Asserts that no errors has been found during the last phase
-        ErrorLogger.assertNoErrors()
-
-        // Sanity checker
-        val sanityChecker = SanityChecker()
-        sanityChecker.visit(ast)
-
-        // Symbol table and scope
-        val scopeChecker = ScopeCheckVisitor()
-        scopeChecker.visit(ast)
-        val symbolTable = scopeChecker.getSymbolTable()
-        println(symbolTable)
-        ErrorLogger.assertNoErrors()
-
-        // Type checking
-        TypeChecker(symbolTable).visit(ast)
-        ErrorLogger.assertNoErrors()
-
+        compileSource(path)
     } catch (e: TerminatedCompilationException) {
 
         // Compilation failed due to errors in program code
@@ -51,16 +83,11 @@ fun compile(path: Path) {
 
     } catch (e: Exception) {
 
-        // Critical error happened, maybe something is be wrong in the compiler
-        // Printing stack trace and errors for debugging reasons
+        // Printing stack trace and errors for debugging purposes
         e.printStackTrace()
         System.err.println("Critical error occurred. Maybe something is wrong in the compiler. Emptying ErrorLogger:")
         ErrorLogger.printAllErrors(path)
     }
-}
-
-fun main() {
-    compile(path)
 }
 
 /**
