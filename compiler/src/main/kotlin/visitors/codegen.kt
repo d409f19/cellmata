42f7152c86ac68a04892ed79e4ea945046f35acd
Jacob Jenner Rasmussen <jacob.mindstorms@gmail.com>
2019-05-08 10:49:24 +0200

    Kotlin codegen
    Added code generator targeting Kotlin.

---
+++ compiler/src/main/kotlin/visitors/codegen.kt
@@ -0,0 +1,620 @@
+package dk.aau.cs.d409f19.cellumata.visitors
+
+import dk.aau.cs.d409f19.cellumata.ast.*
+import java.util.*
+
+sealed class KotlinCodegenError: Error()
+
+class KotlinCodegen_ErrorDecl: KotlinCodegenError()
+
+class KotlinCodegen_ErrorExpr: KotlinCodegenError()
+
+class KotlinCodegen_ErrorStmt: KotlinCodegenError()
+
+class KotlinCodegen_InvalidIdentLookup: KotlinCodegenError()
+
+class KotlinCodegen_FoundUncheckedType: KotlinCodegenError()
+
+class KotlinCodegen_FoundUndefinedWorldType: KotlinCodegenError()
+
+class KotlinCodegen: ASTVisitor<String> {
+    private val INDENT = "    "
+    private var stateIDs: Map<String, Int> = mutableMapOf()
+    private var labelCounter = 0
+
+    private var mapping: Stack<Map<String, String>> = Stack()
+
+    init {
+        mapping.push(mutableMapOf())
+    }
+
+    private fun nextLabel(): String {
+        return "label_${labelCounter++}"
+    }
+
+    private fun addMapping(from: String, to: String) {
+        val oldMapping = mapping.pop()
+        val newMapping = oldMapping.plus(Pair(from, to))
+        mapping.push(newMapping)
+    }
+
+    private fun getMappedLabel(ident: String): String {
+        for (scope in mapping.reversed()) {
+            if (scope.containsKey(ident)) {
+                return scope.getValue(ident)
+            }
+        }
+
+        throw KotlinCodegen_InvalidIdentLookup()
+    }
+
+    private fun openScope() {
+        mapping.push(mapOf())
+    }
+
+    private fun closeScope() {
+        mapping.pop()
+    }
+
+    private fun emitDispatcher(): String {
+        val builder = StringBuilder()
+        builder.appendln("""
+            override fun updateCell(worldView: IWorldView): Int {
+            ${INDENT}return when(worldView.getCell(0, 0)) {
+        """.trimIndent())
+
+        stateIDs.forEach { (_, id) ->
+            builder.appendln("$INDENT$INDENT$id -> state_$id(worldView)")
+        }
+
+        builder.append("""
+            $INDENT${INDENT}else -> throw Error()
+            $INDENT}
+            }
+        """.trimIndent())
+
+        return builder.toString()
+    }
+
+    private fun emitMain(world: WorldNode, states: List<StateDecl>): String {
+        val builder = StringBuilder()
+
+        builder.append("""
+            fun main() {
+                GraphicalDriver(WorldConfiguration(listOf(
+        """.trimIndent())
+
+        world.dimensions.forEachIndexed { i, it ->
+            if (i > 0) {
+                builder.append(", ")
+            }
+            builder.append(it.size)
+        }
+
+        builder.append("), listOf(")
+        states.forEachIndexed { i, it ->
+            if (i > 0) {
+                builder.append(", ")
+            }
+            builder.append("StateColor(")
+            builder.append(it.red)
+            builder.append(", ")
+            builder.append(it.green)
+            builder.append(", ")
+            builder.append(it.blue)
+            builder.append(")")
+        }
+        builder.append("), ")
+        builder.append(if (world.cellSize == null) 2 else world.cellSize)
+        builder.append(", ")
+        builder.append(if (world.tickrate == null) 500 else world.tickrate)
+        builder.append("), ProgramImpl(), ")
+
+        builder.append("MultiWorldType(")
+
+        val edge = world.dimensions[0].edge!! // ToDo make edge a seperate property in world configuration
+        builder.append(stateIDs[edge.spelling])
+        builder.append(", listOf(")
+
+        world.dimensions.forEachIndexed { i, it ->
+            if(i > 0) {
+                builder.append(", ")
+            }
+            builder.append(when(it.type) {
+                WorldType.WRAPPING -> "DimensionType.WRAPPING"
+                WorldType.EDGE -> "DimensionType.EDGE"
+                WorldType.UNDEFINED -> throw KotlinCodegen_FoundUndefinedWorldType()
+            })
+        }
+
+        builder.append("""))).run()
+            }
+        """.trimIndent())
+
+        return builder.toString()
+    }
+
+    private fun toKotlinType(type: Type): String {
+        return when(type) {
+            IntegerType -> "Int"
+            FloatType -> "Float"
+            BooleanType -> "Bool"
+            StateType -> "Int"
+            LocalNeighbourhoodType -> "List<Int>"
+            is ArrayType -> "MutableList<${toKotlinType(type.subtype!!)}>"
+            UncheckedType -> throw KotlinCodegen_FoundUncheckedType()
+        }
+    }
+
+    override fun visit(node: RootNode): String {
+        node.world.dimensions[0].type
+
+        var stateCounter = 0
+        node.body.filter { it is StateDecl }.map { it as StateDecl }.forEach {
+            // ToDo multi-states
+            stateIDs = stateIDs.plus(pair = Pair(it.ident, stateCounter))
+            addMapping(it.ident, stateCounter.toString())
+            stateCounter += 1
+        }
+
+
+        openScope()
+
+        node.body.filter { it !is StateDecl }.forEach {
+            when(it) {
+                is ConstDecl -> addMapping(it.ident, nextLabel())
+                is NeighbourhoodDecl -> addMapping(it.ident, nextLabel())
+                is FuncDecl -> addMapping(it.ident, nextLabel())
+                is ErrorDecl -> throw KotlinCodegen_ErrorDecl()
+            }
+        }
+
+        val builder = StringBuilder()
+        builder.appendln("""
+            package dk.aau.cs.d409f18.cellumata.codegen.kotlin.impl
+
+            import dk.aau.cs.d409f18.cellumata.codegen.kotlin.runtime.*
+
+            class ProgramImpl: IProgram {
+            """.trimIndent())
+
+        builder.appendln(emitDispatcher().prependIndent(INDENT))
+        node.body.forEach { builder.appendln(visit(it).prependIndent(INDENT) + ";") }
+
+        builder.append("""
+            }
+        """.trimIndent())
+
+        closeScope()
+
+        builder.append("\n\n")
+        builder.appendln(emitMain(
+            node.world,
+            node.body.filter { it is StateDecl }.map { it as StateDecl }.toList()
+        ))
+
+        return builder.toString()
+    }
+
+    override fun visit(node: Decl): String {
+        return when(node) {
+            is ConstDecl -> visit(node)
+            is StateDecl -> visit(node)
+            is NeighbourhoodDecl -> visit(node)
+            is FuncDecl -> visit(node)
+            is ErrorDecl -> throw KotlinCodegen_ErrorDecl()
+        }
+    }
+
+    override fun visit(node: ConstDecl): String {
+        return "val ${getMappedLabel(node.ident)} = ${visit(node.expr)};"
+    }
+
+    override fun visit(node: StateDecl): String {
+        // ToDo multi-state
+
+        val builder = StringBuilder()
+
+        builder.appendln("""
+            fun state_${stateIDs[node.ident]}(worldView: IWorldView): Int {
+        """.trimIndent())
+
+        builder.append(visit(node.body).prependIndent(INDENT))
+
+        builder.append("}")
+
+        return builder.toString()
+    }
+
+    override fun visit(node: NeighbourhoodDecl): String {
+        val builder = StringBuilder()
+
+        // Neighbourhood lookup function
+        builder.appendln("""
+            fun ${getMappedLabel(node.ident)}(worldView: IWorldView): List<Int> {
+                return listOf(
+            """.trimIndent())
+
+        node.coords.forEachIndexed { i, it ->
+            builder.append("${INDENT}worldView.getCell(")
+            it.axes.forEachIndexed { o, that ->
+                if (o > 0) {
+                    builder.append(",")
+                }
+                builder.append(that)
+            }
+            builder.append(")")
+            if (i < node.coords.size-1) {
+                builder.append(",")
+            } else {
+                builder.appendln()
+            }
+        }
+
+        builder.append("""
+            $INDENT)
+            }
+        """.trimIndent())
+
+        return builder.toString()
+    }
+
+    override fun visit(node: Coordinate): String {
+        // Should be unreachable
+        TODO("not implemented")
+    }
+
+    override fun visit(node: FuncDecl): String {
+        val builder = StringBuilder()
+
+        openScope()
+        node.args.forEach { addMapping(it.ident, nextLabel()) }
+
+        openScope()
+        builder.append("fun ${getMappedLabel(node.ident)}(")
+        node.args.forEachIndexed { i, it ->
+            if(i > 0) {
+                builder.append(", ")
+            }
+            builder.append("${getMappedLabel(it.ident)}: ${toKotlinType(it.getType()!!)}")
+        }
+        builder.appendln("): ${toKotlinType(node.returnType)} {")
+        builder.append(visit(node.body).prependIndent(INDENT))
+
+        closeScope() // Body
+        closeScope() // Function arguments
+
+        builder.append("}")
+
+        return builder.toString()
+    }
+
+    override fun visit(node: Expr): String {
+        return when(node) {
+            is BinaryExpr -> visit(node)
+            is NegationExpr -> visit(node)
+            is NotExpr -> visit(node)
+            is ArrayLookupExpr -> visit(node)
+            is SizedArrayExpr -> visit(node)
+            is ArrayLiteralExpr -> visit(node)
+            is Identifier -> visit(node)
+            is FuncCallExpr -> visit(node)
+            is StateIndexExpr -> visit(node)
+            is IntLiteral -> visit(node)
+            is BoolLiteral -> visit(node)
+            is FloatLiteral -> visit(node)
+            is IntToFloatConversion -> visit(node)
+            is StateArrayToLocalNeighbourhoodConversion -> visit(node)
+            is ErrorExpr -> throw KotlinCodegen_ErrorExpr()
+        }
+    }
+
+    override fun visit(node: BinaryExpr): String {
+        return when(node) {
+            is BinaryArithmeticExpr -> visit(node)
+            is NumericComparisonExpr -> visit(node)
+            is BinaryBooleanExpr -> visit(node)
+        }
+    }
+
+    override fun visit(node: BinaryArithmeticExpr): String {
+        return when(node) {
+            is AdditionExpr -> visit(node)
+            is SubtractionExpr -> visit(node)
+            is MultiplicationExpr -> visit(node)
+            is DivisionExpr -> visit(node)
+            is ModuloExpr -> visit(node)
+        }
+    }
+
+    override fun visit(node: BinaryBooleanExpr): String {
+        return when(node) {
+            is OrExpr -> visit(node)
+            is AndExpr -> visit(node)
+        }
+    }
+
+    override fun visit(node: NumericComparisonExpr): String {
+        return when(node) {
+            is InequalityExpr -> visit(node)
+            is EqualityExpr -> visit(node)
+            is GreaterThanExpr -> visit(node)
+            is GreaterOrEqExpr -> visit(node)
+            is LessThanExpr -> visit(node)
+            is LessOrEqExpr -> visit(node)
+        }
+    }
+
+    override fun visit(node: OrExpr): String {
+        return "(${visit(node.left)} || ${visit(node.right)})"
+    }
+
+    override fun visit(node: AndExpr): String {
+        return "(${visit(node.left)} && ${visit(node.right)})"
+    }
+
+    override fun visit(node: InequalityExpr): String {
+        return "(${visit(node.left)} != ${visit(node.right)})"
+    }
+
+    override fun visit(node: EqualityExpr): String {
+        return "(${visit(node.left)} == ${visit(node.right)})"
+    }
+
+    override fun visit(node: GreaterThanExpr): String {
+        return "(${visit(node.left)} > ${visit(node.right)})"
+    }
+
+    override fun visit(node: GreaterOrEqExpr): String {
+        return "(${visit(node.left)} >= ${visit(node.right)})"
+    }
+
+    override fun visit(node: LessThanExpr): String {
+        return "(${visit(node.left)} < ${visit(node.right)})"
+    }
+
+    override fun visit(node: LessOrEqExpr): String {
+        return "(${visit(node.left)} <= ${visit(node.right)})"
+    }
+
+    override fun visit(node: AdditionExpr): String {
+        return "(${visit(node.left)} + ${visit(node.right)})"
+    }
+
+    override fun visit(node: SubtractionExpr): String {
+        return "(${visit(node.left)} - ${visit(node.right)})"
+    }
+
+    override fun visit(node: MultiplicationExpr): String {
+        return "(${visit(node.left)} * ${visit(node.right)})"
+    }
+
+    override fun visit(node: DivisionExpr): String {
+        return "(${visit(node.left)} / ${visit(node.right)})"
+    }
+
+    override fun visit(node: NegationExpr): String {
+        return "(-${visit(node.value)})"
+    }
+
+    override fun visit(node: NotExpr): String {
+        return "(!${visit(node.value)})"
+    }
+
+    private fun emitArray(sizes: List<Int>?, type: Type, values: ArrayLiteralExpr?): String {
+        assert(sizes != null && values != null)
+
+        val builder = StringBuilder()
+        var indexCounter = 0
+        val limit: Int = if (sizes == null) {
+            values!!.size!!
+        } else {
+            sizes[0]
+        }
+
+        while (indexCounter < limit) {
+            builder.append(if (values != null && indexCounter < values.values.size) {
+                // Emit values from source code
+                when(values.getType()) {
+                    IntegerType -> visit(values.values[indexCounter])
+                    FloatType -> visit(values.values[indexCounter])
+                    BooleanType -> visit(values.values[indexCounter])
+                    StateType -> visit(values.values[indexCounter])
+                    LocalNeighbourhoodType -> visit(values.values[indexCounter])
+                    is ArrayType -> emitArray(
+                        if (values == null) sizes!!.subList(1, sizes.size) else null,
+                        type,
+                        values.values[indexCounter] as ArrayLiteralExpr
+                    )
+                    UncheckedType -> throw KotlinCodegen_FoundUncheckedType()
+                    null -> TODO()
+                }
+            } else {
+                // Emit default value
+                when (type) {
+                    BooleanType -> "(false)"
+                    IntegerType -> "(0)"
+                    FloatType -> "(0.0F)"
+                    StateType -> "(0)"
+                    LocalNeighbourhoodType -> "(listOf())"
+                    is ArrayType -> emitArray(
+                        if (values == null) sizes!!.subList(1, sizes.size) else null,
+                        type,
+                        null
+                    )
+                    UncheckedType -> throw KotlinCodegen_FoundUncheckedType()
+                }
+            })
+
+            indexCounter++
+        }
+
+        return builder.toString()
+    }
+
+    override fun visit(node: ArrayLookupExpr): String {
+        return "(${visit(node.arr)}[${visit(node.index)}])"
+    }
+
+    override fun visit(node: SizedArrayExpr): String {
+        return emitArray(node.declaredSize.map { it!! }, node.declaredType, node.body)
+    }
+
+    override fun visit(node: Identifier): String {
+        return "(${getMappedLabel(node.spelling)})"
+    }
+
+    override fun visit(node: ModuloExpr): String {
+        return "(${visit(node.left)} % ${visit(node.right)})"
+    }
+
+    override fun visit(node: FuncCallExpr): String {
+        return "(${getMappedLabel(node.ident)}())"
+    }
+
+    override fun visit(node: ArrayLiteralExpr): String {
+        return emitArray(null, node.getType()!!, node)
+    }
+
+    override fun visit(node: StateIndexExpr): String {
+        /*
+         * Idea: Emit a when statement that know based on the state id
+         * which multi-state the state belongs to and the multi-state index
+         */
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(node: IntLiteral): String {
+        return "(${node.value})"
+    }
+
+    override fun visit(node: BoolLiteral): String {
+        return "(${node.value})"
+    }
+
+    override fun visit(node: Stmt): String {
+        return when(node) {
+            is AssignStmt -> visit(node)
+            is IfStmt -> visit(node)
+            is ForLoopStmt -> visit(node)
+            is BreakStmt -> visit(node)
+            is ContinueStmt -> visit(node)
+            is BecomeStmt -> visit(node)
+            is ReturnStmt -> visit(node)
+            is ErrorStmt -> throw KotlinCodegen_ErrorStmt()
+        }
+    }
+
+    override fun visit(node: AssignStmt): String {
+        val label = nextLabel()
+        if (node.isDeclaration) {
+            addMapping(node.ident, label)
+        }
+        return "var $label = (${visit(node.expr)})"
+    }
+
+    override fun visit(node: IfStmt): String {
+        val builder = StringBuilder()
+
+        node.conditionals.forEachIndexed { i, it ->
+            if(i > 0) {
+                builder.append(' ')
+            }
+            builder.append(visit(it))
+        }
+
+        if (node.elseBlock != null) {
+            builder.appendln(" else {")
+            builder.append(visit(node.elseBlock).prependIndent(INDENT))
+            builder.append('}')
+        }
+
+        return builder.toString()
+    }
+
+    override fun visit(node: BecomeStmt): String {
+        return "return ${visit(node.state)}"
+    }
+
+    override fun visit(node: ReturnStmt): String {
+        return "return ${visit(node.value)}"
+    }
+
+    override fun visit(node: AST): String {
+        return when (node) {
+            is RootNode -> visit(node)
+            is WorldNode -> visit(node)
+            is Expr -> visit(node)
+            is Stmt -> visit(node)
+            is Decl -> visit(node)
+            is Coordinate -> visit(node)
+            is FunctionArgument -> visit(node)
+            is ConditionalBlock -> visit(node)
+            is CodeBlock -> visit(node)
+            is ErrorAST -> visit(node)
+        }
+    }
+
+    override fun visit(node: FloatLiteral): String {
+        // String representation may not be exact, so we encode the float as bits to get an exact representation
+        return "(Float.fromBits(${node.value.toBits()}))"
+    }
+
+    override fun visit(node: ConditionalBlock): String {
+        val builder = StringBuilder()
+
+        builder.append("if (${visit(node.expr)}) {\n${visit(node.block).prependIndent(INDENT)}\n}")
+
+        return builder.toString()
+    }
+
+    override fun visit(node: FunctionArgument): String {
+        // Should be unreachable
+        TODO("not implemented")
+    }
+
+    override fun visit(node: WorldNode): String {
+        // Should be unreachable
+        TODO("not implemented")
+    }
+
+    override fun visit(node: WorldDimension): String {
+        // Should be unreachable
+        TODO("not implemented")
+    }
+
+    override fun visit(node: ForLoopStmt): String {
+        val builder = StringBuilder()
+
+        builder.appendln(node.initPart)
+
+        builder.append("while (")
+        builder.append(visit(node.condition))
+        builder.append(") {")
+
+        builder.appendln(visit(node.body))
+        if (node.postIterationPart != null) {
+            builder.appendln(visit(node.postIterationPart))
+        }
+
+        builder.append("}")
+
+        return builder.toString()
+    }
+
+    override fun visit(node: BreakStmt): String {
+        return "break"
+    }
+
+    override fun visit(node: ContinueStmt): String {
+        return "continue"
+    }
+
+    override fun visit(node: CodeBlock): String {
+        val builder = StringBuilder()
+
+        node.body.forEach { builder.append(visit(it)); builder.appendln(";") }
+
+        return builder.toString()
+    }
+}
